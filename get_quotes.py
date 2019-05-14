1f8c72be1fd6768ed3ee6444ebcba65f5e9cd4fd
Shankusu7 <pankaj08072000@gmail.com>
2019-05-14 10:39:38 +0530

    satisfy pep8 standards

--- get_quotes.py
+++ get_quotes.py
@@ -1,15 +1,24 @@
+"""
+    Scrapes Quotes for Developers
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
-scrape_url = "https://fortrabbit.github.io/quotes/"
 
-url_raw_data = requests.get(scrape_url)
-data = url_raw_data.text
-soup = BeautifulSoup(data, 'html5lib')
-quotes = []
-for block in soup.find_all('blockquote', attrs={'class': "hero-quote third half-t"}):
-    quote = {}
-    quote["quote"] = block.p.text.strip()
-    quote["author"] = block.div.text
-    quotes.append(quote)
-print(len(quotes))
+def get_quotes():
+    """Returns a lit of scrapped quotes"""
+
+    scrape_url = "https://fortrabbit.github.io/quotes/"
+    url_raw_data = requests.get(scrape_url)
+    data = url_raw_data.text
+    soup = BeautifulSoup(data, 'html5lib')
+    quotes = []
+
+    for block in soup.find_all('blockquote', attrs={'class': "hero-quote third half-t"}):
+        quote = {}
+        quote["quote"] = block.p.text.strip()
+        quote["author"] = block.div.text
+        quotes.append(quote)
+
+    return quotes
